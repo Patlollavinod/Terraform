9e5453819e168a34dd12a40abe1120980980bb2b
Patlollavinod <99877597+Patlollavinod@users.noreply.github.com>
2022-06-28 11:13:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "app" {
     instance_type       = "t2.micro"
     ami                 = "ami-068257025f72f470d"
     availability_zone   = "ap-south-1a"
+    tag _name           = "p_server"
 }
 resource "aws_lb_target_group" "my_target_group" {
     health_check {
