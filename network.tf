cb95ab3cafe82ac563d2871021d400e7edc3943a
Patlollavinod <99877597+Patlollavinod@users.noreply.github.com>
2022-06-28 15:17:33 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -121,7 +121,7 @@ resource "aws_route_table" "privatert" {
 
 resource "aws_route" "internet_access" {
 
-  route_table_id         = aws_vpc.patlolla.aws_route_table.privatert.id
+  route_table_id         = aws_route_table.privatert.id
 
   destination_cidr_block =  local.any_where
 
